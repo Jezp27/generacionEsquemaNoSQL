89a54a64c18a0e0a657c5fec255425a71c4800d6
Jenny Zapata <jenny.zapatap.96@gmail.com>
2020-10-20 01:20:21 -0500

    Initial Commit

---
+++ src/solucion/Main.java
@@ -0,0 +1,18 @@
+package solucion;
+
+import Formularios.Regla1;
+
+/**
+ *
+ * @author Jenny Zapata, Juan David Ordoñez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Regla1 vista1 = new Regla1();
+        vista1.setVisible(true);
+    } 
+}
